c6c25b12e49898e6969a50384f37bb0b97cb4926
pablo <dalbem@gmail.com>
2023-03-07 13:27:51 -0300

    Exercícios resolvidos

--- Associação entre Classes/ExercicioPlaylist/src/Main.java
+++ Associação entre Classes/ExercicioPlaylist/src/Main.java
@@ -3,19 +3,20 @@ import java.time.LocalTime;
 public class Main {
     public static void main(String[] args) {
 
-        Musica m1 = new Musica("Musica1","artista",
-                LocalTime.of(0,3,20),"rock");
+        Musica m1 = new Musica("Espelho","João Nogueira",
+                LocalTime.of(0,4,29),"samba");
         m1.mostrarDados();
 
-        Musica m2 = new Musica("Musica2", "artista",
-                LocalTime.of(0,3,50),"rock");
+        Musica m2 = new Musica("O Mundo é um Moinho", "Cartola",
+                LocalTime.of(0,3,53),"samba");
         m2.mostrarDados();
 
-        Playlist playlist = new Playlist("Minha playlist");
+        Playlist playlist = new Playlist("Sambas");
         playlist.adiconarMusica(m1);
         playlist.adiconarMusica(m2);
         playlist.mostrarDados();
-        System.out.println("Duracao: "+ playlist.calcularDuracao());
+        System.out.println("Tempo total da playlist "+ playlist.getNome()
+                       +": " + playlist.calcularDuracao());
 
     }
 }
